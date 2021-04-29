401aae2f591d7c8d2edfcc65dfaf85de192cdc7b
AhhEagle <dim2though@gmail.com>
2021-04-29 13:24:36 +0100

    modified orders table sql command

--- migrations/sqls/20210429051955-orders-table-up.sql
+++ migrations/sqls/20210429051955-orders-table-up.sql
@@ -3,5 +3,7 @@ CREATE TABLE orders
 (
     id SERIAL PRIMARY KEY,
     status VARCHAR(15),
+    quantity INTEGER ,
+    product_id INTEGER REFERENCES products(id),
     user_id integer REFERENCES users(id)
 );
--- migrations/sqls/20210429052007-orders-products-table-up.sql
+++
@@ -1,8 +0,0 @@
-/* Replace with your SQL commands */
-CREATE TABLE order_products
-(
-    id SERIAL PRIMARY KEY,
-    quantity integer,
-    order_id integer REFERENCES orders(id),
-    product_id integer REFERENCES products(id)
-);
